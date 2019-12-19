46f2f2c21fd73841f8108cd62cdda1d4ec48eb66
unfernandito <lfbu.112@gmail.com>
2019-12-18 22:34:58 -0300

    docs folder

---
+++ docs/precache-manifest.0b43220f12fdb6bc16c3e6303e65b038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6791b9262ce6b5fe4502a29efce0199",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ff3d111d5ea3d27d0e4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a471b86f39194c267168",
+    "url": "/static/js/2.2c72e4ea.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.2c72e4ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ff3d111d5ea3d27d0e4",
+    "url": "/static/js/main.bc419746.chunk.js"
+  },
+  {
+    "revision": "a95c9e62145a2f2dcd53",
+    "url": "/static/js/runtime-main.0b679ac1.js"
+  }
+]);
