491234b347bba84c6b3478e11e5144e886e218c1
unfernandito <lfbu.112@gmail.com>
2019-12-25 14:59:53 -0300

    Updates

---
+++ precache-manifest.b69d771a9f2cc1aec4d6d514b60629a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bd516c2a26ac49b363a24011de419f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "22db67458b248713ec87",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3d007c26538716b8e3e8",
+    "url": "/portfolio/static/js/2.36a03968.chunk.js"
+  },
+  {
+    "revision": "374a015f9bd7dbb4f757a46f1c6a7946",
+    "url": "/portfolio/static/js/2.36a03968.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22db67458b248713ec87",
+    "url": "/portfolio/static/js/main.2737f384.chunk.js"
+  },
+  {
+    "revision": "453dfe7de3f697ab5e8c",
+    "url": "/portfolio/static/js/runtime-main.95a61890.js"
+  }
+]);
